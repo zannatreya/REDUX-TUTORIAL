12c2f6ac08cf4a861a8312a939a580441963c395
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-20 20:53:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const { createStore } = require("redux");
+
+// defining constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+// const ADD_USER="ADD_USER";
+
+//state
+const initialCounterState = {
+    count: 0,
+};
+
+// const initialUsersState = {
+// users: [{name: "Zannatul Binta Bahar"}],
+// };
+
+// action - Object- type, payload
+const incrementCounter = () => {
+    return {
+      type: INCREMENT,
+    };
+  };
+  
+  const decrementCounter = () => {
+    return {
+      type: DECREMENT,
+    };
+  };
+
+  // const AddUser =()=>{
+  //   return {
+  //     type: ADD_USER,
+  //     payload:{name:"zannat"},
+  //   }
+  // }
+
+
+// Reducer
+
+const counterReducer =(state=initialCounterState,action)=>{
+switch (action.type) {
+  case INCREMENT:
+    return{
+      ...state,
+      count: state.count+1,
+    }
+  case DECREMENT:
+    return{
+      ...state,
+      count: state.count-1,
+    }
+
+  default:
+    state;
+  }
+};
+
+//create store
+
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{
+  console.log(store.getState());
+})
+
+//dispatch action
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
