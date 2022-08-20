b18b58b9330130821aacb11953c9af5ed55275c1
zannatreya <zannatulbintabahar@yahoo.com>
2022-08-20 21:46:40 +0600

    payload updated

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { createStore } = require("redux");
 
 // defining constants
-const INCREMENT = "INCREMENT";
-const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
-const DECREMENT = "DECREMENT";
-const RESET = "RESET";
+// const INCREMENT = "INCREMENT";
+// const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+// const DECREMENT = "DECREMENT";
+// const RESET = "RESET";
+  const ADD_USER="ADD_USER";
+
 // const ADD_USER="ADD_USER";
 
 //state
 const initialCounterState = {
-    count: 0,
+    users: ["zannat"],
+    count: 1,
 };
 
 // const initialUsersState = {
@@ -17,60 +20,73 @@ const initialCounterState = {
 // };
 
 // action - Object- type, payload
-const incrementCounter = () => {
-    return {
-      type: INCREMENT,
-    };
-  };
+// const incrementCounter = () => {
+//     return {
+//       type: INCREMENT,
+//     };
+//   };
   
-  const decrementCounter = () => {
-    return {
-      type: DECREMENT,
-    };
-  };
-  const resetCounter = () => {
-    return {
-      type: RESET,
-    };
-  };
-  const incrementCounterByValue = (value) => {
-    return {
-      type: INCREMENT_BY_VALUE,
-      payload: value,
-    };
-  };
-
-  // const AddUser =()=>{
+  // const decrementCounter = () => {
   //   return {
-  //     type: ADD_USER,
-  //     payload:{name:"zannat"},
-  //   }
-  // }
+  //     type: DECREMENT,
+  //   };
+  // };
+  // const resetCounter = () => {
+  //   return {
+  //     type: RESET,
+  //   };
+  // };
+  // const incrementCounterByValue = (value) => {
+  //   return {
+  //     type: INCREMENT_BY_VALUE,
+  //     payload: value,
+  //   };
+  // };
+
+  const addUser =(user)=>{
+    return {
+      type: ADD_USER,
+      payload:user,
+    }
+  }
 
 
 // Reducer
 
-const counterReducer =(state=initialCounterState,action)=>{
+// const counterReducer =(state=initialCounterState,action)=>{
+// switch (action.type) {
+//   case INCREMENT:
+//     return{
+//       ...state,
+//       count: state.count+1,
+//     }
+//   case DECREMENT:
+//     return{
+//       ...state,
+//       count: state.count-1,
+//     }
+//   case RESET:
+//     return{
+//       ...state,
+//       count: 0,
+//     }
+//   case INCREMENT_BY_VALUE:
+//     return{
+//       ...state,
+//       count: state.count + action.payload,
+//     }
+
+//   default:
+//     state;
+//   }
+// };
+const userReducer =(state=initialCounterState,action)=>{
 switch (action.type) {
-  case INCREMENT:
+  
+  case ADD_USER:
     return{
-      ...state,
-      count: state.count+1,
-    }
-  case DECREMENT:
-    return{
-      ...state,
-      count: state.count-1,
-    }
-  case RESET:
-    return{
-      ...state,
-      count: 0,
-    }
-  case INCREMENT_BY_VALUE:
-    return{
-      ...state,
-      count: state.count + action.payload,
+      users: [...state.users,action.payload],
+      count: state.count + 1,
     }
 
   default:
@@ -80,7 +96,7 @@ switch (action.type) {
 
 //create store
 
-const store = createStore(counterReducer);
+const store = createStore(userReducer);
 
 store.subscribe(()=>{
   console.log(store.getState());
@@ -93,5 +109,7 @@ store.subscribe(()=>{
 // store.dispatch(decrementCounter());
 // store.dispatch(resetCounter());
 // store.dispatch(incrementCounter());
-store.dispatch(incrementCounterByValue(10));
-store.dispatch(incrementCounterByValue(5));
+// store.dispatch(incrementCounterByValue(10));
+// store.dispatch(incrementCounterByValue(5));
+store.dispatch(addUser("reya"));
+store.dispatch(addUser("ibrar"));
